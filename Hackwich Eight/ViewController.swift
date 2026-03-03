8c5c87a647f68c62cabaacc9939bf84abeaa27b3
Hunter Trujillo <megafan.exe@gmail.com>
2026-03-02 23:12:58 -1000

    Created and programmed tables and arrays. Will ask for help with completion in class.

--- Hackwich Eight/ViewController.swift
+++ Hackwich Eight/ViewController.swift
@@ -7,11 +7,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource
+{
+    
+    @IBOutlet weak var friendTable: UITableView!
+    
+    
+    var friends: [String] = ["Diego", "Royce", "Brandon"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return friends.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell: UITableViewCell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+        cell.textLabel?.text = friends[indexPath.row]
+        return cell
+    }
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
--- Hackwich Eight/ViewController2.swift
+++ Hackwich Eight/ViewController2.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController2.swift
+//  ViewController.swift
 //  Hackwich Eight
 //
 //  Created by Hunter Trujillo on 3/2/26.
@@ -7,23 +7,34 @@
 
 import UIKit
 
-class ViewController2: UIViewController {
+class ViewController2: UIViewController, UITableViewDelegate, UITableViewDataSource
+{
+    
+    
+    @IBOutlet weak var travelTable: UITableView!
+    
+    
+    var places: [String] = ["Korea", "Japan", "Disneyland"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return places.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell: UITableViewCell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+        cell.textLabel?.text = places[indexPath.row]
+        return cell
+    }
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
+
